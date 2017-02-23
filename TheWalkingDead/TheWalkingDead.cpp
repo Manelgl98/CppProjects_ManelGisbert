677bf685c3a124296e3bb4bdcfcbff241fce9d53
manelgisbertlopez <manelgisbertlopez@S3-09>
2017-02-23 13:43:46 +0100

    inacabado0.1

--- TheWalkingDead/TheWalkingDead.cpp
+++ TheWalkingDead/TheWalkingDead.cpp
@@ -2,10 +2,89 @@
 //
 
 #include "stdafx.h"
+#include<iostream>
+#include<string>
+#include<ctime>
+
+const int cantidad = 10;
+enum class Weapon {
+	FISTS,
+	GUN,
+	SHOTGUN,
+	REVOLVER,
+	SNIPER,
+	MACHINE_GUN,
+	MAX
+};
+class Zombie;
+
+class Player {
+public:
+	Weapon weapon;
+	float precision;
+	int life;
+	void attack(Zombie&);
+	bool isAlive(Player&);
+	void randGenerator();
+};
+
+class Zombie {
+public:
+	int distanceToPlayer;
+	float speed;
+	float damange;
+	int life;
+private:
+	void attack(Player&);
+	bool isAlive(Zombie&);
+	void randGenerator();
+	
+};
+void Player::attack(Zombie& z) {
+	if (precision == 1.f) {
+		z.life = z.life - static_cast<int>(weapon);
+	}
+}
+bool Zombie::isAlive(Zombie& z) {
+	return(z.life > 0);
+}
+
+bool Player::isAlive(Player& p) {
+	return(p.life > 0);
+}
+
+void Zombie::attack(Player& p) {
+	if (distanceToPlayer <= 0) {
+		p.life -= ;
+	}
+}
+void Player::randGenerator() {
+	
+}
+void Zombie::randGenerator() {
+	Zombie{
+	rand() % 201,
+	rand() % 21,
+	rand() % 21,
+	rand()%101
+	};
+}
+	
 
 
 int main()
 {
+	srand(time(nullptr));
+	Player player;
+	Zombie zombie[cantidad];
+	bool zombiesAreAlive;
+	while (player.life && zombie > 1) {
+		zombiesAreAlive = false;
+		for (int i = 0; i < 10; i++) {
+			
+		}
+
+	}
     return 0;
 }
 
