e9589320a81c40e2efc97f175edf77f3751d3b51
manelgisbertlopez <manelgisbertlopez@S3-05>
2017-04-27 13:51:29 +0200

    actividad 1 y 2 funciona

--- STLdequeQueuePriorydeque/STLdequeQueuePriorydeque.cpp
+++ STLdequeQueuePriorydeque/STLdequeQueuePriorydeque.cpp
@@ -7,56 +7,56 @@
 #include<vector>
 #include<string>
 #include<iostream>
-int main()
+void main()
 {
 	//1
-	/*
+	
 	std::deque<float>prices(10, 10.5f);//Creo una deque de 10 elementos de 10.5
 	prices[9] = 32.43f;//Cambio el ultimo por 32.43
 	prices.pop_front();//Borro el primer elemento
-	std::deque<float>d;
-	d.assign(prices.begin, prices.end);//asigno la deque en una nueva deque
+	std::deque<float>d(prices);
+	
 	d.insert(d.begin(),5.64f); //inserto 5.64 al principio de la segunda deque
 	d.insert(d.begin(), 20.31f);//inserto 20.31 al principio de la segunda deque
 	d.insert(d.begin() + 1, 10);//inserto el valor 10 en la segunda posicion
 	//imprimimos primera deque
-	std::deque<int>::iterator it;
+
 	std::cout << "Primera deque";
-	for (it = prices.begin(); it != prices.end(); ++it)
-		std::cout << " " << &it;
+	for (auto it = prices.rbegin(); it != prices.rend(); ++it)
+		std::cout << " " << *it;
 	std::cout <<std:: endl;
 	
 	//imprimimos segunda deque
-	std::deque<int>::iterator it2 = d.begin;
+	
 	std::cout << "Segunda deque";
-	for (it2 = d.begin(); it2 != d.end(); ++it2)
-		std::cout << " " << &it2;
+	for (auto it2 = d.begin(); it2 != d.end(); ++it2)
+		std::cout << " " << *it2;
 	std::cout << std::endl;
-	*/
+	
 	//2
-	std::vector<float>pricesv(10, 10.5f);
-	pricesv[9] = 32.43f;
+	std::vector<float>pricesv(10, 10.5);
+	pricesv[9] = 32.43;
 	pricesv.erase(pricesv.begin());
-	std::vector<float>dv;
-	dv.assign(pricesv.begin, pricesv.end);
-	dv.insert(pricesv.begin(), 5.64f);
-	dv.insert(pricesv.begin(), 32.43f);
-	dv.insert(pricesv.begin() + 1, 10);
+	std::vector<float>dv(pricesv);
+	
+	dv.insert(dv.begin(), 5.64);
+	dv.insert(dv.begin(), 32.45);
+	dv.insert(dv.begin() + 1, 10);
 
 	//imprimimos primer vector
 	std::cout << "Primer vector";
-	std::vector<int>::reverse_iterator rit = pricesv.rbegin();
-	for(;rit!=pricesv.rend;++rit)
-		std::cout << " " << *rit;
+	
+	for(auto it =pricesv.rbegin();it != pricesv.rend();++it)
+		std::cout << " " << *it;
 	std::cout << std::endl;
 	//imprimimos segundo vector
 	std::cout << "Segundo vector";
-	for (auto it = dv.begin; !dv.end; ++it)
-		std::cout << " " << *it;
+	for (auto it2 = dv.begin();it2 != dv.end(); ++it2)
+		std::cout << "" << *it2;
 	std::cout << std::endl;
-
+	
 	//3
-	/*
+	
 	std::priority_queue<std::string> contactQueue(std::priority_queue<std::string> a, std::priority_queue<std::string> b) {
 
 	}
@@ -64,8 +64,8 @@ int main()
 
 	}
 
-	*/
+	
 
-    return 0;
+    
 }
 
