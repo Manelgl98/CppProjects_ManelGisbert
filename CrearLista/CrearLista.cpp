ae13bcc5ce784cac4b88c6115c0950d992d4efc8
manelgisbertlopez <manelgisbertlopez@S3-05>
2017-05-16 11:13:10 +0200

    nueva practica

---
+++ CrearLista/CrearLista.cpp
@@ -0,0 +1,43 @@
+// CrearLista.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include"Lista.h"
+
+myList::myList() :
+	
+{};
+void myList::front() {
+	
+}
+void myList::back() {
+
+}
+
+void myList::push_front() {
+	
+}
+
+void myList::push_back() {
+
+}
+
+void myList::pop_front() {
+
+}
+
+void myList::pop_back() {
+
+}
+
+void myList::size() {
+
+}
+void myList::isEmpty() {
+
+}
+int main()
+{
+    return 0;
+}
+
