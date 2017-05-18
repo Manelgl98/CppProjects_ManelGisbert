2aa4ce66a3a706f900e4d0c17629ae3e67e577ca
Manel Gisbert López <mgisbertlopez@gmail.com>
2017-05-18 16:44:06 +0100

    Main cambiado

--- DynArray/main.cc
+++ DynArray/main.cc
@@ -1,9 +1,15 @@
-#include "stdafx.h"
-#include <ostream>
 #include "DynArray.hh"
+#include <iostream>
 
 void main() {
-	DynArray *array;
-	array = new DynArray();
+	DynArray ar1;
 
+	int arr[10]{ 1 };
+	DynArray ar2(arr, 10);
+
+	ar1 = ar2;
+	std::cout << ar2[0];
+
+	int *i = new int[5];
+	int test = i - i + 3;
 }
